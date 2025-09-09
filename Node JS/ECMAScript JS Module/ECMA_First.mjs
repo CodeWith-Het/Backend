738406473631df28aea83da63dc8cfdeac3b98b3
CodeWith-Het <hetp1065@gmail.com>
2025-09-09 12:50:34 -0400

    useFul information

--- Node JS/ECMAScript JS Module/ECMA_First.mjs
+++ Node JS/ECMAScript JS Module/ECMA_First.mjs
@@ -1,3 +1,9 @@
 import { simple } from './ECMA_Second.mjs'
 
 simple();
+
+
+
+//  * Jab Tum ECMAScript JS Module ka use karte ho Tab Ye Bat DHyan me leni hai
+//* -->package.json 
+//* type: "module"  ye edit karna padta hai package.json me pakcage-lock.json me kuch bhi nahi karna hai
