a1fcc2b782b8896fc475a8193a7ae1ea337d1acd
CodeWith-Het <hetp1065@gmail.com>
2025-09-23 12:49:31 -0500

    Updated 23/09/2025

--- Day 04/Statics.js
+++ Day 04/Statics.js
@@ -9,8 +9,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));    
 
 app.get('/', (req, res) => {
-    res.render('static'); // make sure static.ejs is inside the 'views' folder
+    res.render('static'); 
 });
+
+app.get('/backend',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'javascripts', 'scripts.js'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
